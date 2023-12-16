1abf566c319bc4c91bf3615015d8e9ecf301086c
Kyle Johnson <66799208+Gladius2k@users.noreply.github.com>
2023-12-15 22:50:46 -0800

    Small changes to .ejs files and added more comments

--- login.js
+++ login.js
@@ -1,7 +1,10 @@
 //Kyle Johnson
 //KidsCyberAcademy Login-Code
+
 //Beta. Very simplistic will be changing to work in Wix at a later date, main usage is to facilitate login and encryption processes. 
-//This code runs on node.js, which connects to an express server and allows the user to login, save the username and password, and keeps in encrypted until it is decrypted to check 
+//This code runs on node.js, which connects to an express server and allows the user to login, save the username and password, and keeps in encrypted until it is decrypted to check
+// This code is built upon a view on creating a Passport Node JS login https://www.youtube.com/watch?v=-RCnNyD0L-s, however I have added my own design, pages, and attributes into my 
+// own unqiue version
 
 // libraries and file calls
 const express = require('express') //Temp server, will intergrate with Wix by the end of project
@@ -12,15 +15,16 @@ const app = express()// This also
 const bcrypt = require('bcrypt')
 const passportID = require('passport')
 
-if (process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production')
+{
     require('dotenv').config()
 }
 
 //Connect this to a databse, do not leave for final project
 const users = []
 
-const initPassport = require('./passport')
-initPassport(passportID, email => 
+const initPassport = require('./passport') 
+    initPassport(passportID, email => 
     users.find(user => user.email === email),
     id => users.find(user=>user.id === id)
 
@@ -37,45 +41,64 @@ app.use(session({
     resave: false,
     saveUninitialized: false
 }))
-app.use(passportID.initialize())
+app.use(passportID.initialize())//initialize a passport ID session
 app.use(passportID.session())
 app.use(methodOverride('_method'))
 
 
-app.get('/', ifAuth, (req, res) => {
+app.get('/', ifAuth, (req, res) => 
+{
     res.render('index.ejs', {name: req.user.name})
 })
 
 
 //Different calls when the page is needed to then pull up the html .ejs file for that page
-app.get('/loginChoice', (req, res) => {
+app.get('/loginChoice', (req, res) => 
+{
     res.render('loginChoice.ejs')
 })
-app.get('/student_login', (req, res) => {
+app.get('/student_login', (req, res) => 
+{
     res.render('student_login.ejs')
 })
-app.get('/teacher_login', ifAuthCurrent,(req, res) => {
+app.get('/teacher_login', ifAuthCurrent,(req, res) => 
+{
     res.render('teacher_login.ejs')
 })
-app.get('/student_signup', (req, res) => {
+app.get('/student_signup', (req, res) => 
+{
     res.render('student_signup.ejs')
 })
-app.get('/teacher_signup', ifAuthCurrent,(req, res) => {
+app.get('/teacher_signup', ifAuthCurrent,(req, res) => 
+{
     res.render('teacher_signup.ejs')
+})
+app.get('/loginChoice', ifAuthCurrent,(req,res)  => 
+{
+    res.render('loginChoice.ejs')
 })
 
 //calls for requests and responses to inputs on the pages. 
-//app.post('/student_login', (req, res) => {
+//app.post('/student_login', (req, res) => 
+//{
     //req.body.userName
   //  req.body.password
 //})
+app.post('/loginChoice', ifAuthCurrent,passportID.authenticate('local', 
+{
+    successRedirect: '/',
+    failureRedirect: '/loginChoice',
+    failureFlash: true
+}))
 
-app.post('/student_login', ifAuthCurrent,passportID.authenticate('local', {
+/*app.post('/student_login', ifAuthCurrent,passportID.authenticate('local', 
+{
     successRedirect: '/',
     failureRedirect: '/student_login',
     failureFlash: true
-}))
-app.post('/student_signup', async(req, res) => {
+}))*/
+app.post('/student_signup', async(req, res) => 
+{
     
     try{
         //add part  for password checker
@@ -98,12 +121,14 @@ app.post('/student_signup', async(req, res) => {
     }  
     console.log(users)
 })
-app.post('/teacher_login', ifAuthCurrent,passportID.authenticate('local', {
+app.post('/teacher_login', ifAuthCurrent,passportID.authenticate('local', 
+{
     successRedirect: '/',
     failureRedirect: '/teacher_login',
     failureFlash: true
 }))
-app.post('/teacher_signup', ifAuthCurrent,async(req, res) => {
+app.post('/teacher_signup', ifAuthCurrent,async(req, res) => 
+{
     try{
         //add part  for password checker
         //req.body.password
@@ -132,7 +157,8 @@ app.post('/teacher_signup', ifAuthCurrent,async(req, res) => {
     req.body.school
 })
 //Simple logout button, will change later
-app.delete('/logout', (req, res, next) =>{
+app.delete('/logout', (req, res, next) =>
+{
     req.logOut((err)=>{
         if (err){
             return next (err);
@@ -143,7 +169,8 @@ app.delete('/logout', (req, res, next) =>{
 
     
 //Check if the user is authenticated. If they are not, instead of just going to any link they want, it will send them to the login page. #Change to login choice page#
-function ifAuth(req, res, next) {
+function ifAuth(req, res, next) 
+{
     if (req.isAuthenticated()){
         return next()
     }
@@ -151,7 +178,8 @@ function ifAuth(req, res, next) {
     res.redirect('/teacher_login')
 }
 //Check if the authenticated. IF they are, makes sure they cannot go back to a page if they are already logined in. 
-function ifAuthCurrent(req, res, next){
+function ifAuthCurrent(req, res, next)
+{
     if (req.isAuthenticated()){
         return res.redirect('/')
     }
