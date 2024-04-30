9383de3db8bd192de2622b5125d6a9989ad52212
Looney-James <jlooney88@nevada.unr.edu>
2024-04-30 12:24:03 -0700

    Badge Assignment

--- Wix Development/masterPage.js
+++ Wix Development/masterPage.js
@@ -4,6 +4,7 @@ import wixLocation from 'wix-location';
 import wixData from 'wix-data';
 
 import { currentMember, authentication } from 'wix-members-frontend';
+import { badgeAssign } from 'backend/badgeAssign';
 
 $w.onReady(async function () {
     let member;
@@ -14,8 +15,11 @@ $w.onReady(async function () {
             $w("#masterButton").text = "Log Out"
             $w("#missionButton").show();
 
+            
+
             const student = await wixData.query("MainDatabase").eq("email",member.loginEmail).eq("isStudent", true).find();
             const teacher = await wixData.query("MainDatabase").eq("email",member.loginEmail).eq("isTeacher", true).find();
+            const admin = await wixData.query("MainDatabase").eq("email",member.loginEmail).eq("isAdmin", true).find();
 
             if (student.items.length > 0) {
 
@@ -31,6 +35,23 @@ $w.onReady(async function () {
             } else {
                 $w("#teacherDashboard").hide();
             }
+
+                if (admin.items.length > 0) {
+
+                $w("#adminDashboard").show();
+
+            } else {
+                $w("#adminDashboard").hide();
+
+            }
+
+            if(student.items.length > 0 && student.items[0].l1Complete && student.items[0].l2Complete){
+
+                const badgeId = "f416c2cf-9269-48e5-948a-6f16b980900e";
+
+                badgeAssign(badgeId, member._id);
+            }
+
         } else {
             wixLocation.to("/register");
         }
